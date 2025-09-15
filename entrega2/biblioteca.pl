cfdb8bd6911019da7805befb6e166a3bcc3ecf81
martigagna <martinagagna@gmail.com>
2025-09-15 22:17:52 +0000

    Ejercicios entrega 2 - Finalizados

--- entrega2/biblioteca.pl
+++ entrega2/biblioteca.pl
@@ -33,19 +33,21 @@ listar_libros :-
 listar_libros.  % Clausula vacía para terminar
 
 % Eliminar un libro
+
+% Caso 1: el libro existe y no está prestado
 eliminar_libro(Titulo) :-
     libro(Titulo, _),
     \+ prestado(Titulo, _),
     retract(libro(Titulo, _)),
     write('Libro eliminado: '), write(Titulo), nl.
 
-% El libro esta prestado
+% Caso 2: el libro está prestado
 eliminar_libro(Titulo) :-
     prestado(Titulo, _),
     write('Error: Libro '), write(Titulo), write(' está prestado.'), nl,
     fail. % Fuerza fracaso si está prestado
 
-% El libro no existe
+% Caso 3: el libro no existe
 eliminar_libro(Titulo) :-
     \+ libro(Titulo, _),
     write('Error: Libro '), write(Titulo), write(' no existe.'), nl,
@@ -53,21 +55,39 @@ eliminar_libro(Titulo) :-
 
 % Ejercicio adicional: eliminar préstamo
 eliminar_prestamo(Titulo) :-
-    retract(prestado(Titulo, _)),
-    write(' Préstamo eliminado para libro: '), write(Titulo), nl.
+    retract(prestado(Titulo, Persona)),
+    write(' Préstamo eliminado para libro: '), write(Titulo),
+    write(' (estaba prestado a '), write(Persona), write(')'), nl.
+
+eliminar_prestamo(Titulo) :-
+    \+ prestado(Titulo, _),
+    write('Error: no hay préstamo registrado para el libro '), write(Titulo), nl,
+    fail.
+
+% Prueba eliminar préstamo
+% ?- eliminar_prestamo("el_principito").
+% Salida: Préstamo eliminado para libro: el_principito (estaba prestado a juan)
+%         true 
+
+% ?- eliminar_prestamo("1984").
+% Salida: Error: no hay préstamo registrado para el libro 1984
+%         false.
 
 
 % Pruebas con eliminar_libro
 
 % ?- eliminar_libro("1984").
+% Salida: Libro eliminado: 1984
 % Primero Prolog busca en la base de hechos si existe libro("1984", Autor), como si existe
 % verifica que no exista prestado("1984", _), no hay entonces continua y ejecuta 
 % retract(libro("1984", _)) y elimina el hecho de la base.
-% Resultado: Libro eliminado: 1984
+
 
 % eliminar_libro("el_principito").
+% Salida: Error: Libro el_principito está prestado.
+%         false.
 % Primero Prolog busca en la base de hechos si existe libro("el_principito", Autor), como si existe
 % verifica si esta prestado y encuentra prestado("el_principito", "juan").
-% Debido a que el libro esta prestado, entra en la segunda clausula y muestra el mensaje de error.
-% Resultado: Error: Libro el_principito está prestado.
-%            false.
+% Debido a que el libro esta prestado, entra en el segundo caso por lo tanto no elimina el libro e imprime el mensaje de error. 
+
+
